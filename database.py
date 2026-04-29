b3546553821b8596844b1aaee72f12c14128e679
Domurah <221390869+domurah98@users.noreply.github.com>
2026-04-29 17:37:40 +0300

    api

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="app_db"
+    host="gateway01.us-east-1.prod.aws.tidbcloud.com",
+    user="4BtnYynyA2LKuR9.root",
+    password="ehCOCdwW7MinZKIj",
+    database="sys"
 )
 
 cursor = db.cursor(dictionary=True)
